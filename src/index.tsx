730ed810e8abd9baa1226b1bbd09816a0fe43233
safarov-en <safarov.elkhan110@gmail.com>
2024-07-30 21:51:57 +0500

    Adding a router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/pages/AccountSettingsPage/AccountSettingsPage.tsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet"
+
+const AccountSettingsPage: React.FC = () => {
+    return (
+        <>
+            <Helmet>
+                <title>Настройка аккаунта - MW Marketplace</title>
+            </Helmet>
+            <h1>Настройка аккаунта</h1>
+        </>
+    )
+}
+
+export default AccountSettingsPage
